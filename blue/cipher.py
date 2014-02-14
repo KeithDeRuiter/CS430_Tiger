f69c07f68af154ba0aaf7a00b1982fdf6f058bab
CS402 <cs402@CS402.(none)>
2014-02-13 16:38:57 -0800

    added a homophonic cipher and commented the cipher.py file

--- blue/cipher.py
+++ blue/cipher.py
@@ -1,25 +1,27 @@
 import random
 import copy
-thing=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ','.','!','?']
+#list of the alphabet we are using
+alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ','.','!','?']
 random.seed(10)
-listofthings=[]
+#list of 100 monoalphabetic keys
+keyList=[]
+#this copies the alphabet and then shuffles it 100 times for 100 keys
 for x in xrange(0,100):
-  thing2=copy.deepcopy(thing)
-  listofthings.append(thing2)
-  random.shuffle(listofthings[x])
-thing3="this is a message abcdefghijklmnopqrstuvwxyz !?."
-thing4=""
-for x in xrange(0,100):
-  print(listofthings[x])
-print(thing3)
+  thing=copy.deepcopy(alphabet)
+  keyList.append(thing)
+  random.shuffle(keyList[x])
+message="this is a message abcdefghijklmnopqrstuvwxyz !?."
+encrypted=""
+print(message)
 t=0
-for letter in thing3:
-  thing4+=(listofthings[t][thing.index(letter)])
+#encrypts message polyalphabetically (doing it monoalphabetically just means you dont increment t)
+for letter in message:
+  encrypted+=(keyList[t][alphabet.index(letter)])
   t=t+1
-print(thing4)
-thing5=""
+print(encrypted)
+decrypted=""
 t=0
-for letter in thing4:
-  thing5+=thing[listofthings[t].index(letter)]
+for letter in encrypted:
+  decrypted+=alphabet[keyList[t].index(letter)]
   t=t+1
-print(thing5)
+print(decrypted)
