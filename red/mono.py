561d71a2700771493d6c85c8e63151b6dd0e03e8
Juanchen Li <chen@guest-wireless-upc-nat-206-117-88-092.usc.edu>
2014-02-17 23:48:48 -0800

    almost finished monoalphabetic cipher, still need to connect

--- red/mono.py
+++ red/mono.py
@@ -1,15 +1,132 @@
 import string
+import copy
 
-with open("words.txt") as word_file:
-	word_list = set(word.strip().lower() for word in word_file)
+# maps a->0, b->1, ect
+def to_num(char):
+	return ord(char)-97
 
-# checks if word is an english word
-def is_word(word):
-	return word.lower() in word_list
+def to_char(num):
+	return chr(num+97)
 
-message = "IVK SV NT IKTT MZ WVS CTKTFP SV ORZS VII VWTZ OGRMWZ, NDS SV FMET MW R BRP SGRS KTZXTOSZ RWY TWGRWOTZ SGT IKTTYVC VI VSGTKZ.".lower()
-cipher = "".join(l for l in message if l not in string.punctuation)
+pattern_dict = {}
 
-key = {}
+# read the pattern dictionary
+with open('pattern_dict.txt') as pattern_file:
+	lines = pattern_file.readlines()
+	is_pattern = True;
+	cur_pattern = '';
+	for line in lines:
+		line = line.rstrip()
+		if is_pattern:
+			is_pattern = False
+			cur_pattern = line
+			pattern_dict[cur_pattern] = set()
+		else:
+			if line == "#":
+				is_pattern = True
+			else:
+				pattern_dict[cur_pattern] |= {line}
 
-print cipher
+message = 'LY LZ RIYYIV YF PIQJ KVFD RINLHJ QHJ YF OSY FYNIVZ LH KVFHY, IZOIALQPPU XNIH UFS AIPIRVQYI CLAYFVU XNIH HLAI YNLHTZ FAASV. UFS YQGI YNI KVFHY PLHI XNIH YNIVI LZ JQHTIV. YNIH OIFOPI XLPP QOOVIALQYI UFSV PIQJIVZNLO'.lower()
+cipher = ''.join(l for l in message if l not in string.punctuation)
+words = cipher.split(' ')
+
+print message
+
+# set up the key to have every possibility
+key = []
+
+for letter in 'abcdefghijklmnopqrstuvwxyz':
+	alphabet = set()
+
+	if letter in cipher:
+		for l in 'abcdefghijklmnopqrstuvwxyz':
+			alphabet.add(l)
+		key.append(alphabet)
+	else:
+		alphabet.add('-')
+		key.append(alphabet)
+
+# the initial dictionary attack run through
+# for every word in the cipher
+for word in words:
+	count = 'A'
+	pattern = word
+
+	# determine the pattern
+	for letter in pattern:
+		if letter.islower():
+			pattern = pattern.replace(letter, count);
+			count = chr(ord(count)+1)
+
+	# for every letter in the word, determine the possible mappings based on dictionary patterns
+	letter_id = 0
+	for letter in word:
+		possible_set = set()c
+		# for every possibility (possible word determined by the given pattern)
+		for possibility in pattern_dict[pattern]:
+			possible_set.add(possibility[letter_id])
+		letter_id += 1
+		key[to_num(letter)] = key[to_num(letter)] & possible_set
+
+
+
+# refine 10 times, if its not finished after 10 times you might as well give up
+for i in range(10):
+
+	# remove letters you have already sloved from the rest of the possibilities
+	for i in range(26):
+		if len(key[i]) == 1:
+			for j in range(26):
+				if len(key[j]) > 1:
+					key[j] = key[j] - key[i]
+
+	for word in words:
+		count = 'A'
+		pattern = word
+
+		for letter in pattern:
+			if letter.islower():
+				pattern = pattern.replace(letter, count);
+				count = chr(ord(count)+1)
+
+		possible_words = copy.deepcopy(pattern_dict[pattern])
+
+		# get the letters we have already solved
+		solved_letters = []
+		solved_word = ''
+		for i in range(len(word)):
+			if len(key[to_num(word[i])]) == 1:
+				solved_letters.append(key[to_num(word[i])])
+			else:
+				solved_letters.append(set(['-']))
+
+		for i in solved_letters:
+			for e in i:
+				solved_word += e
+
+		# find the words that don't matched the new pattern
+		to_remove = set()
+		for wrd in possible_words:
+			for i in range(len(wrd)):
+				if solved_word[i] != '-' and solved_word[i] != wrd[i]:
+					to_remove.add(wrd)
+					break
+
+		# filter possibilities based on new information
+		letter_id = 0
+		for letter in word:
+			possible_set = set()
+			for possibility in possible_words - to_remove:
+				possible_set.add(possibility[letter_id])
+			letter_id += 1
+			key[to_num(letter)] = key[to_num(letter)] & possible_set
+
+
+# solve the cipher message
+i = 0
+for answer in key:
+	message = message.replace(to_char(i), answer.pop().upper())
+	i += 1
+
+print message
