9575c72aed832e4f00a54e6b5153b116770b8c1e
KeithDeRuiter <keithderuiter@gmail.com>
2014-02-26 14:06:33 -0800

    fixed sniffer

--- red/sniffer.py
+++ red/sniffer.py
@@ -1,9 +1,14 @@
 import nfqueue
-import mono
+#import mono
+import socket
 from dpkt import ip, tcp
 
 q = None
 seq = 0
+
+def incrementSeq():
+    global seq
+    seq = seq + 1
 
 def cb(dummy, payload):
     #callback
@@ -18,12 +23,14 @@ def cb(dummy, payload):
     #print '--------'
 
     if len(packetData) > 0:
+        print "Message received"
         filename = 'message_' + str(seq) + '.txt'
-        seq = seq + 1
+        incrementSeq()
         f = open(filename, 'w')
         f.write(packetData)
         print packetData
         f.close()
+        print "Message written to file"
 
     payload.set_verdict(nfqueue.NF_ACCEPT)
 
@@ -38,6 +45,10 @@ try:
     q.try_run()
 except KeyboardInterrupt:
     print "Exiting..." 
+finally:    #Need to ensure that socket is unbound, even on interrupt
+    q.unbind(socket.AF_INET)
+    q.close()
+    print "Unbound"
 
 q.unbind(socket.AF_INET)
 q.close()
